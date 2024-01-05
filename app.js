e6c97a1a21dc8380f2db92037330a6fcd974359e
Thummala Sujith <srt7@njit.edu>
2024-01-04 20:39:54 -0500

    completed button

---
+++ app.js
@@ -0,0 +1,40 @@
+// Selectors
+
+const { todo } = require("node:test");
+
+const todoInput=document.querySelector(".todo-input");
+const todoButton=document.querySelector(".todo-button")
+const todoList=document.querySelector(".todo-list")
+
+
+
+
+
+//Event Listeners
+
+todoButton.addEventListener('click',addTodo);
+
+//Functions
+
+function addTodo(event){
+
+event.preventDefault();
+
+
+// Todo Div
+const todoDiv=document.createElement("div");
+todoDiv.classList.add('todo');
+
+//create li
+const newTodo=document.createElement("li");
+newTodo.innerText="hey";
+newTodo.classList.add('todo-item');
+todoDiv.appendChild(newTodo);
+
+//Check Mark button
+
+const completedButton=document.createElement('button');
+completedButton.innerHTML='<i class="fas fa-check"></i>';
+completedButton.classList.add("completed-btn")
+todoDiv.appendChild(completedButton)
+}
